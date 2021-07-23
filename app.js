116c2837a8e5efe7f9270545779a7689c266ebb2
ajcoldi <63015086+ajcoldi@users.noreply.github.com>
2021-07-23 18:32:16 +0100

    debug

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.render('index.html')
+    res.render('index')
 })
 
 
